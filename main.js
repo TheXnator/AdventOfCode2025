8760c33c8999474afd27fe63e82fa7657a4fdb00
TheXnator <thexnator@thexnator.dev>
2025-12-04 02:03:47 +0000

    sample data

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-function runDay(day, dataFile) {
-    const dataPath = path.join(__dirname, 'data', dataFile);
+function runDay(day, dataFile, useSample) {
+    const dataPath = path.join(__dirname, useSample ? 'sample-data' : 'data', dataFile);
 
     let inputData = fs.readFileSync(dataPath, 'utf-8');
     let dayModule = require('./day' + day + '.js');
 
     console.log("");
-    console.log("Running Day " + day);
+    console.log("Running Day " + day + (useSample ? " (Sample Data)" : ""));
     let resultPart1 = dayModule.part1(inputData);
     console.log("Part 1 Result: " + resultPart1);
     let resultPart2 = dayModule.part2(inputData);
@@ -17,7 +17,13 @@ function runDay(day, dataFile) {
 }
 
 // Run days
-const RUN_DAYS = [1, 2];
+const RUN_SINGLE_DAY = null;
+const RUN_DAYS = [
+    { day: 1, useSample: false },
+    { day: 2, useSample: true },
+];
+
 RUN_DAYS.forEach(day => {
-    runDay(day, 'day' + day + '.txt');
+    if (RUN_SINGLE_DAY !== null && RUN_SINGLE_DAY !== day.day) return;
+    runDay(day.day, 'day' + day.day + '.txt', day.useSample);
 });

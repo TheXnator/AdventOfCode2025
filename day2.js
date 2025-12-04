a65774aa5321ffe9c932b6c82fd4b6b6ad0b1e59
TheXnator <thexnator@thexnator.dev>
2025-12-04 01:52:05 +0000

    tweak run

---
+++ day2.js
@@ -0,0 +1,9 @@
+function part1(inputData) {
+    return "A"
+}
+
+function part2(inputData) {
+    return "A"
+}
+
+module.exports = { part1, part2 };
--- main.js
+++ main.js
@@ -7,12 +7,17 @@ function runDay(day, dataFile) {
     let inputData = fs.readFileSync(dataPath, 'utf-8');
     let dayModule = require('./day' + day + '.js');
 
+    console.log("");
     console.log("Running Day " + day);
     let resultPart1 = dayModule.part1(inputData);
     console.log("Part 1 Result: " + resultPart1);
     let resultPart2 = dayModule.part2(inputData);
     console.log("Part 2 Result: " + resultPart2);
+    console.log("");
 }
 
 // Run days
-runDay(1, 'day1.txt');
+const RUN_DAYS = [1, 2];
+RUN_DAYS.forEach(day => {
+    runDay(day, 'day' + day + '.txt');
+});
